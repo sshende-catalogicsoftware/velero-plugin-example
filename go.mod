07b5c77e00e00cf4f9a64ec832e5dab470caacfc
Swanand Shende <sshende@catalogicsoftware.com>
2021-03-03 22:09:12 +0530

    Updated the dependencies in go.mod

--- go.mod
+++ go.mod
@@ -3,25 +3,18 @@ module catalogicsoftware.com/velero-plugin
 go 1.14
 
 require (
-	github.com/Azure/go-autorest/autorest v0.10.0 // indirect
-	github.com/Azure/go-autorest/autorest/adal v0.8.3 // indirect
-	github.com/gogo/protobuf v1.3.0 // indirect
 	github.com/hashicorp/go-hclog v0.9.2 // indirect
 	github.com/hashicorp/go-plugin v1.0.1-0.20190610192547-a1bc61569a26 // indirect
 	github.com/hashicorp/yamux v0.0.0-20190923154419-df201c70410d // indirect
 	github.com/imdario/mergo v0.3.9 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/mitchellh/go-testing-interface v1.0.0 // indirect
-	github.com/pkg/errors v0.8.1
+	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v0.0.5 // indirect
 	github.com/vmware-tanzu/velero v1.3.2
-	golang.org/x/sys v0.0.0-20191007154456-ef33b2fb2c41 // indirect
-	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
-	google.golang.org/genproto v0.0.0-20191007162740-aa923e3a3354 // indirect
-	google.golang.org/grpc v1.19.1 // indirect
-	k8s.io/api v0.0.0-20190620084959-7cf5895f2711
-	k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
-	k8s.io/client-go v0.0.0-20190620085101-78d2af792bab
+	k8s.io/api v0.20.4
+	k8s.io/apimachinery v0.20.4
+	k8s.io/client-go v0.20.0
 	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
 )
--- internal/plugin/util/util.go
+++ internal/plugin/util/util.go
@@ -17,11 +17,11 @@ limitations under the License.
 package util
 
 import (
+	"context"
 	"fmt"
 	"strconv"
 
 	"github.com/pkg/errors"
-	"github.com/vmware-tanzu/velero/pkg/label"
 	"github.com/vmware-tanzu/velero/pkg/plugin/framework"
 	corev1api "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -29,64 +29,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/retry"
 )
-
-// GetPVForPVC returns a PV object bound to a PVC
-func GetPVForPVC(pvc *corev1api.PersistentVolumeClaim, corev1 corev1client.PersistentVolumesGetter) (*corev1api.PersistentVolume, error) {
-	if pvc.Spec.VolumeName == "" {
-		return nil, errors.Errorf("PVC %s/%s has no volume backing this claim", pvc.Namespace, pvc.Name)
-	}
-	if pvc.Status.Phase != corev1api.ClaimBound {
-		// TODO: confirm if this PVC should be snapshotted if it has no PV bound
-		return nil, errors.Errorf("PVC %s/%s is in phase %v and is not bound to a volume", pvc.Namespace, pvc.Name, pvc.Status.Phase)
-	}
-	pvName := pvc.Spec.VolumeName
-	pv, err := corev1.PersistentVolumes().Get(pvName, metav1.GetOptions{})
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to get PV %s for PVC %s/%s", pvName, pvc.Namespace, pvc.Name)
-	}
-	return pv, nil
-}
-
-//GetPodsUsingPVC lists all pods where this PVC is used
-func GetPodsUsingPVC(pvcNamespace, pvcName string, corev1 corev1client.PodsGetter) ([]corev1api.Pod, error) {
-	podsUsingPVC := []corev1api.Pod{}
-	podList, err := corev1.Pods(pvcNamespace).List(metav1.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	for _, p := range podList.Items {
-		for _, v := range p.Spec.Volumes {
-			if v.PersistentVolumeClaim != nil && v.PersistentVolumeClaim.ClaimName == pvcName {
-				podsUsingPVC = append(podsUsingPVC, p)
-			}
-		}
-	}
-
-	return podsUsingPVC, nil
-}
-
-// GetPodVolumeNameForPVC returns the volume name in a POD spec for a particular PVC
-func GetPodVolumeNameForPVC(pod corev1api.Pod, pvcName string) (string, error) {
-	for _, v := range pod.Spec.Volumes {
-		if v.PersistentVolumeClaim != nil && v.PersistentVolumeClaim.ClaimName == pvcName {
-			return v.Name, nil
-		}
-	}
-	return "", errors.Errorf("Pod %s/%s does not use PVC %s/%s", pod.Namespace, pod.Name, pod.Namespace, pvcName)
-}
-
-// Contains checks if a key is present in a slice
-func Contains(slice []string, key string) bool {
-	for _, i := range slice {
-		if i == key {
-			return true
-		}
-	}
-	return false
-}
 
 // GetClients creates and returns a kubernetes clientset
 func GetClients() (*kubernetes.Clientset, error) {
@@ -103,46 +46,6 @@ func GetClients() (*kubernetes.Clientset, error) {
 		return nil, errors.WithStack(err)
 	}
 	return client, nil
-}
-
-// AddAnnotations adds the supplied key-values to the annotations on the object
-func AddAnnotations(o *metav1.ObjectMeta, vals map[string]string) {
-	if o.Annotations == nil {
-		o.Annotations = make(map[string]string)
-	}
-	for k, v := range vals {
-		o.Annotations[k] = v
-	}
-}
-
-// AddLabels adds the supplied key-values to the labels on the object
-func AddLabels(o *metav1.ObjectMeta, vals map[string]string) {
-	if o.Labels == nil {
-		o.Labels = make(map[string]string)
-	}
-	for k, v := range vals {
-		o.Labels[k] = label.GetValidName(v)
-	}
-}
-
-// UpdatePvAnnotation updates the Persistent Volume Resource
-func UpdatePvAnnotation(key, value, pvcName string, client *kubernetes.Clientset) error {
-	pvClient := client.CoreV1().PersistentVolumes()
-	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		// Retrieve the latest version of PV before attempting update
-		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-		result, getErr := client.CoreV1().PersistentVolumes().Get(pvcName, metav1.GetOptions{})
-		if getErr != nil {
-			return getErr
-		}
-		result.GetAnnotations()[key] = value
-		_, updateErr := pvClient.Update(result)
-		return updateErr
-	})
-	if retryErr != nil {
-		return retryErr
-	}
-	return nil
 }
 
 // ParseResourceRequirements takes a set of CPU and memory requests and limit string
@@ -202,6 +105,7 @@ func ParseResourceRequirements(cpuRequest, memRequest, cpuLimit, memLimit string
 	return resources, nil
 }
 
+// ParseSecurityContext will parse the security context related string values from configmap
 func ParseSecurityContext(runAsUser string, runAsGroup string, allowPrivilegeEscalation string) (corev1api.SecurityContext, error) {
 	securityContext := corev1api.SecurityContext{}
 
@@ -235,12 +139,15 @@ func ParseSecurityContext(runAsUser string, runAsGroup string, allowPrivilegeEsc
 	return securityContext, nil
 }
 
+// GetPluginConfig reads the configmap that contains config params for this plugin
 func GetPluginConfig(kind framework.PluginKind, name string, client corev1client.ConfigMapInterface) (*corev1api.ConfigMap, error) {
 	opts := metav1.ListOptions{
+		// velero.io/plugin-config: true
+		// velero.io/restic: RestoreItemAction
 		LabelSelector: fmt.Sprintf("velero.io/plugin-config,%s=%s", name, kind),
 	}
 
-	list, err := client.List(opts)
+	list, err := client.List(context.TODO(), opts)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
